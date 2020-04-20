116cf90db179f0ad359a4d9f54c4b0eddf5eb137
Daniel Qian <danielqian2015@gmail.com>
2020-04-19 19:05:38 -0700

    Switched to creating a database

--- DEVELOPMENT.md
+++ DEVELOPMENT.md
@@ -1,3 +1,5 @@
 # Development
 
 ---
+####4/19/2020
+   Decided to use the SQLiteModernCPP and SQLite3 library.
--- apps/my_app.cc
+++ apps/my_app.cc
@@ -1,13 +1,26 @@
 // Copyright (c) 2020 [Your Name]. All rights reserved.
 
 #include "my_app.h"
-#include "AudioUnit.h"
 #include <cinder/app/App.h>
+#include <algorithm>
+#include <chrono>
+#include <cmath>
+#include <string>
 
 
 namespace myapp {
 
 using cinder::app::KeyEvent;
+using cinder::Color;
+using cinder::ColorA;
+using cinder::Rectf;
+//using cinder::TextBox;
+using cinder::app::KeyEvent;
+using std::chrono::duration_cast;
+using std::chrono::seconds;
+using std::chrono::system_clock;
+using std::string;
+
 
 MyApp::MyApp() { }
 
